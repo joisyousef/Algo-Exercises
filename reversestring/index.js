8757c5c6ea8ed6a3ce41d24eb852f98b0b8c1ab2
Yousef Elsrogy <yousefelsrogy500@gmail.com>
2025-09-22 19:16:44 +0300

    reverse string solution 3

--- reversestring/index.js
+++ reversestring/index.js
@@ -15,7 +15,7 @@
 //     return str.split('').reverse().join('');
 // }
 
-// Solution 2
+// // Solution 2
 // function reverse(str) {
 //   let reversed = "";
 //
@@ -25,5 +25,33 @@
 //   return reversed;
 // }
 
+// Solution 3
+function reverse(str) {
+    return str.split("").reduce((rev,char)=>{
+        return char + rev;
+    },'')
+
+// 1. `str.split("")`
+//     - This takes your string and breaks it into individual letters
+//     - Example: "hello" becomes `["h", "e", "l", "l", "o"]`
+//
+// 2. `.reduce((rev, char) => {...}, '')`
+//     This is like a special loop that builds something new by going through each letter:
+//         - = the reversed string we're building (starts as empty `''`) `rev`
+//     - = the current letter we're looking at `char`
+//     - We start with an empty string `''`
+//
+// 3. `return char + rev`
+//     - For each letter, we put it **in front of** what we've built so far
+//     - It's like stacking letters backwards!
+
+    // 1. Start: `rev = ""`, `char = "h"` → return `"h" + "" = "h"`
+    // 2. Next: `rev = "h"`, `char = "e"` → return `"e" + "h" = "eh"`
+    // 3. Next: `rev = "eh"`, `char = "l"` → return `"l" + "eh" = "leh"`
+    // 4. Next: `rev = "leh"`, `char = "l"` → return `"l" + "leh" = "lleh"`
+    // 5. Final: `rev = "lleh"`, `char = "o"` → return `"o" + "lleh" = "olleh"`
+
+
+}
 
 module.exports = reverse;
