e50acfbc1bdc70376c2b10cbd83290a4cdf4ec01
Yousef Elsrogy <yousefelsrogy500@gmail.com>
2025-09-22 18:34:20 +0300

    reverse string solution 1

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,14 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// Solution 1
+function reverse(str) {
+   // const arr = str.split('');
+   // arr.reverse();
+   // return arr.join('');
+
+    return str.split('').reverse().join('');
+}
 
 module.exports = reverse;
